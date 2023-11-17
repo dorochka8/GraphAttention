48d1f9b58226d43c7f0d7bcb301f4f23cd52cb2a
dora <97133490+dorochka8@users.noreply.github.com>
2023-11-17 03:47:23 +0300

    Update MultiHeadGraphAttention.py

--- models/MultiHeadGraphAttention.py
+++ models/MultiHeadGraphAttention.py
@@ -4,21 +4,21 @@ import torch.nn.functional as F
 from GraphAttentionLayer import GraphAttentionLayer
 
 class MultiHeadGraphAttention(nn.Module):
-  def __init__(self, input_dim, output_dim, num_heads, alpha=0.2, dropout=0.0, concat=True):
-    super().__init__()
-    self.heads = nn.ModuleList()
-    self.concat  = concat
-    self.dropout = dropout
-  
-    for i in range(num_heads):
-      self.heads.append(
-          GraphAttentionLayer(input_dim, output_dim, alpha=alpha, dropout=self.dropout)
-          )
+    def __init__(self, input_dim, output_dim, num_heads, alpha=0.2, dropout=0.0, concat=True):
+        super().__init__()
+        self.heads = nn.ModuleList()
+        self.concat  = concat
+        self.dropout = dropout
+    
+        for i in range(num_heads):
+            self.heads.append(
+                GraphAttentionLayer(input_dim, output_dim, alpha=alpha, dropout=self.dropout)
+            )
 
 
-  def forward(self, x, adj):
-    if self.concat:
-      return torch.cat([head(x, adj) for head in self.heads], dim=1)
-      
-    else:
-      return torch.mean(torch.stack([head(x, adj) for head in self.heads]), dim=0)
+    def forward(self, x, adj):
+        if self.concat:
+            return torch.cat([head(x, adj) for head in self.heads], dim=1)
+        
+        else:
+            return torch.mean(torch.stack([head(x, adj) for head in self.heads]), dim=0)
