be8ac337b35bb8b435570e6d272dbb735d61e42c
dora <97133490+dorochka8@users.noreply.github.com>
2023-11-17 16:37:35 +0300

    Update preprocess_data.py

--- datasets/cora/preprocess_data.py
+++ datasets/cora/preprocess_data.py
@@ -1,1 +1,17 @@
+def extracting_info(data):
+    graph = data[0]
 
+    features = graph.ndata['feat']
+    labels   = graph.ndata['label']
+
+    train_mask = graph.ndata['train_mask']
+    val_mask   = graph.ndata['val_mask']
+    test_mask  = graph.ndata['test_mask']
+
+    masks = [train_mask, val_mask, test_mask]
+
+    return graph, features, labels, masks
+
+def to_adjacency(matrix):
+    adjacency_matrix = None
+    return adjacency_matrix
