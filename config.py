ba7696ea671a17b14d7e490eff6a2f345e038048
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-13 22:21:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
-import torch.nn as nn
-
 config = {
-    # optimizer
+    # optimizer parameters
     'lr': 5e-3,
     'weight_decay': 5e-4,
 
-    # loss function 
-    'loss_fn': nn.CrossEntropyLoss(),
-
-    # model set-up
-    'model_alpha'      : 0.2,   # 
-    'model_dropout'    : 0.6,   #  
-    'model_concat'     : True,  # concatenation of attention outputs if False -> *averaging*
+    # model architecture  
+    'model_alpha'      : 0.2,    
+    'model_dropout'    : 0.6,     
     'model_num_heads'  : [8, 1],# number of attention heads
     'model_num_hidden' : 8,     # number of hidden layers
+    
+    # model training
     'train_epochs'     : 200,   # total number of epochs to train
-    'seed'             : 72     
+    'seed'             : 46911356,
+    'threshold'        : 100, 
+    
     }
