af2f576611b7a56e52944ef1fa9e503fb612cdae
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-15 14:33:23 +0300

    Update training.py

--- datasets/cora/training.py
+++ datasets/cora/training.py
@@ -1,68 +1,102 @@
+# import os, sys
+# config_path = '/your/path/to/the/GraphAttention/'
+# sys.path.append(config_path)
+
+import glob
 import numpy as np 
 import random
 import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from preprocess_data import extracted_info, accuracy
 from config import config
+from models.GraphAttentionNetwork import GraphAttentionNetwork
+from preprocess_data import extracted_info, accuracy, modify_adj
 
 
 def training(model, X, y, idx_train, idx_val, adj, optimizer, epochs):
     t_total = time.time()
     loss_values = []
-    best, best_epoch = 0.0, 0
+    best, best_epoch, counter = 0.0, 0, 0
     model.train()
     for epoch in range(epochs):
         epoch_time = time.time()
         optimizer.zero_grad()
-        outputs = model(X[idx_train], adj)
-        loss_train = F.nll_loss(outputs[idx_train], y[idx_train])
+        
+        outputs = model(X[idx_train], modify_adj(adj, idx_train))
+        loss_train = F.cross_entropy(outputs, y[idx_train])
         acc_train  = accuracy(outputs, y[idx_train])
-        loss_values.append(loss_train)
+        loss_values.append(loss_train.item())
 
         loss_train.backward()
         optimizer.step()
         
         with torch.no_grad():
             model.eval()
-            outputs  = model(X, adj)
-            loss_val = F.nll_loss(outputs[idx_val], y[idx_val])
-            acc_val  = accuracy(outputs[idx_val], y[idx_val])
+            outputs  = model(X[idx_val], modify_adj(adj, idx_val))
+            loss_val = F.cross_entropy(outputs, y[idx_val])
+            acc_val  = accuracy(outputs, y[idx_val])
     
-        print(f'Epoch: {epoch+1}')
-        print(f'Train_loss: {loss_train.data.item():.5f} \tTrain_acc: {acc_train.data.item():.5f}')
-        print(f'Val_loss:   {loss_val.data.item():.5f} \tVal_acc: {acc_val.ata.item():.5f}')
-        print(f'time: {(time.time()-epoch_time):.5f}')
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch: {epoch+1}')
+            print(f'Train_loss: {loss_train.data.item():.5f} \tTrain_acc: {acc_train.data.item():.5f}')
+            print(f'Val_loss:   {loss_val.data.item():.5f} \tVal_acc: {acc_val.data.item():.5f}')
+            print(f'time: {(time.time()-epoch_time):.5f}')
 
         torch.save(model.state_dict(), f'{epoch+1}.pkl')
         if loss_values[-1] < best: 
             best = loss_values[-1]
             best_epoch = epoch+1
             counter = 0
-        else:
-            counter += 1
-        if counter == config['threshold']:
+        else: counter += 1
+        if counter == config['patience']:
             break
+            
+        files = glob.glob('*.pkl')
+        for file in files:
+            epoch_nb = int(file.split('.')[0])
+            if epoch_nb < best_epoch:
+                os.remove(file)
+
+    files = glob.glob('*.pkl')
+    for file in files:
+        epoch_nb = int(file.split('.')[0])
+        if epoch_nb > best_epoch:
+            os.remove(file)
+            
+    print(f'time spent: {(time.time()-t_total):.5f}')
+    plt.plot(loss_values)
+    plt.show()
+    return 
 
 
 device = 'cuda' if torch.cuda.is_available else 'cpu'
-
 random.seed(config['seed'])
 np.random.seed(config['seed'])
 torch.manual_seed(config['seed'])
 if device == 'cuda':
     torch.cuda.manual_seed(config['seed'])
+    
+# download the dataset 
+path = "./data"  # change manually to the dir where the data downloaded
+os.makedirs(path, exist_ok=True)
+dataset_name = 'Cora'
+dataset = Planetoid(root=path, name=dataset_name)
+data = dataset[0]
 
-
-idx_train, idx_val, idx_test, adj, features, labels = extracted_info()
-model = None
+idx_train, idx_val, idx_test, adj, features, labels = extracted_info(path, dataset_name)
+model = GraphAttentionNetwork(
+            input_dim=features.shape[1], 
+            output_dim=len(torch.unique(labels)),
+            num_hidden_layers=config['model_num_hidden'], 
+            num_heads=config['model_num_heads'], 
+            alpha=config['model_alpha'],
+            dropout=config['model_dropout'], 
+            )
 optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'] )
 if device == 'cuda':
     model.cuda()
     idx_train, idx_val, idx_test = idx_train.cuda(), idx_val.cuda(), idx_test.cuda()
     adj, features, labels = adj.cuda(), features.cuda(), labels.cuda()
-
 
 training(model, features, labels, idx_train, idx_val, adj, optimizer, config['train_epochs'])
