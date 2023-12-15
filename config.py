8b1701c4eefbe668ee45254950a01d433e2eaceb
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-15 14:39:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ config = {
     'weight_decay': 5e-4,
 
     # model architecture  
-    'model_alpha'      : 0.2,    
+    'model_alpha'      : 0.2, # slope ratio for ELU   
     'model_dropout'    : 0.6,     
-    'model_num_heads'  : [8, 1],# number of attention heads
-    'model_num_hidden' : 8,     # number of hidden layers
+    'model_num_heads'  : 8,   # number of attention heads
+    'model_num_hidden' : 8,   # number of hidden layers
     
     # model training
-    'train_epochs'     : 200,   # total number of epochs to train
+    'train_epochs'     : 200, # total number of epochs to train
     'seed'             : 46911356,
-    'threshold'        : 100, 
-    
+    'patience'         : 100, 
     }
